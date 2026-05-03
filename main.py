a965664f4a8e586515266ca1aa14ce13b1f435b4
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:48 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# sorting-algorithms-visualizer
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Sorting Algorithms Visualizer built with Python")
+    print("Project: sorting-algorithms-visualizer")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
